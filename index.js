6ba990db28eed8ac9ee52b09f4328d5f17f2b347
Alex Bepple <alex@bepple.de>
2021-09-19 20:16:31 +0200

    $ let orders expire in 22 hours

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ import * as r from 'ramda'
 const isIsin = r.pipe(r.length, r.equals(12))
 const findSide = r.find(r.includes(r.__, ['buy', 'sell']))
 
+const msToS = r.pipe(x => x / 1000, x => Math.floor(x))
+const toUnixMs = r.pipe(x => new Date(x), x => x.valueOf())
+const hToMs = x => x * 60 * 60 * 1000
+const validUntilUnixSeconds = r.pipe(
+    // so, this is quite painful :-(((
+    // date-fns – does not work with Node ESM
+    // @js-temporal/polyfill – was not workable
+    // maybe Luxon?
+    () => new Date(), toUnixMs, ms => ms + hToMs(22), toUnixMs, msToS
+)()
+
 const order = {
-    "valid_until": 1632157200,
+    "valid_until": validUntilUnixSeconds,
     "side": findSide(process.argv),
     "isin": r.find(isIsin)(process.argv),
     "limit_price": 40.7,
